8772efbcee066b97d9ae8e89c5de1b7126b5bfe4
mohdthoriq <mohdthoriq@gmail.com>
2026-01-09 14:01:39 +0700

    feat: santri

--- src/repository/santri.repository.ts
+++ src/repository/santri.repository.ts
@@ -5,6 +5,7 @@ export interface ICreateSantriPayload {
   nis: string;
   fullname: string;
   kelas: string;
+  gender: string;
   waliId: number;
   institutionId: number;
 }
@@ -19,21 +20,54 @@ export interface ISantriRepository {
 }
 
 export class SantriRepository implements ISantriRepository {
-  constructor(private prisma: PrismaClient) {}
+  constructor(private prisma: PrismaClient) { }
 
   async create(payload: ICreateSantriPayload): Promise<Santri> {
-    const exists = await this.prisma.santri.findUnique({
-      where: { nis_institutionId: { nis: payload.nis, institutionId: payload.institutionId } },
+    const institution = await this.prisma.institution.findUnique({
+      where: { id: payload.institutionId },
     });
-    if (exists) throw new Error("Santri with this NIS already exists in the institution");
 
-    const santri = await this.prisma.santri.create({ data: payload });
-    return santri;
+    if (!institution) {
+      throw new Error("Institution not found");
+    }
+
+    const exists = await this.prisma.santri.findUnique({
+      where: {
+        nis_institutionId: {
+          nis: payload.nis,
+          institutionId: payload.institutionId,
+        },
+      },
+    });
+
+    if (exists) {
+      throw new Error("Santri with this NIS already exists in the institution");
+    }
+
+
+    return this.prisma.santri.create({
+      data: {
+        nis: payload.nis,
+        fullname: payload.fullname,
+        kelas: payload.kelas,
+        gender: payload.gender,
+        waliId: payload.waliId,
+        institutionId: payload.institutionId,
+      },
+    });
   }
 
   async getList(institutionId: number): Promise<Santri[]> {
     return this.prisma.santri.findMany({
       where: { institutionId },
+      include: {
+        institution: {
+          select: {
+            id: true,
+            name: true,
+          }
+        },
+      },
       orderBy: { createdAt: "desc" },
     });
   }
