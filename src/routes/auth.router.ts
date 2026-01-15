72988ce2436114d84f1953f936be600a5f827097
mohdthoriq <mohdthoriq@gmail.com>
2026-01-15 09:50:27 +0700

    resend-otp

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -3,8 +3,12 @@ import PrismaInstance from "../database";
 import { AuthService } from "../services/auth.service";
 import { AuthController } from "../controllers/auth.controller";
 import { AuthRepository } from "../repository/auth.repository";
+import { EmailVerificationRepository } from "../repository/emailVerification.repository";
+import { EmailVerificationService } from "../services/emailVerification.service";
+import { EmailVerificationController } from "../controllers/emailVerification.controller";
 import { validate } from "../utils/validator";
 import { loginValidation, registerAdminValidation, requestResetValidation, resetPasswordValidation } from "../validations/auth.validation";
+import { requestOtpValidation } from "../validations/emailVerification.validation";
 
 const router = Router();
 
@@ -12,11 +16,16 @@ const repo = new AuthRepository(PrismaInstance);
 const service = new AuthService(repo);
 const controller = new AuthController(service);
 
-router.post("/register-admin",validate(registerAdminValidation),controller.registerAdmin );
-router.post("/login", validate(loginValidation),controller.login);
+const evRepo = new EmailVerificationRepository(PrismaInstance);
+const evService = new EmailVerificationService(evRepo, PrismaInstance);
+const evController = new EmailVerificationController(evService);
 
-router.post("/request-reset", validate(requestResetValidation),controller.requestReset);
-router.post("/reset-password", validate(resetPasswordValidation),controller.resetPassword);
+router.post("/register-admin", validate(registerAdminValidation), controller.registerAdmin);
+router.post("/login", validate(loginValidation), controller.login);
+router.post("/resend-otp", validate(requestOtpValidation), evController.resendOtpPublic);
+
+router.post("/request-reset", validate(requestResetValidation), controller.requestReset);
+router.post("/reset-password", validate(resetPasswordValidation), controller.resetPassword);
 
 
 export default router;
--- src/routes/emailVerification.router.ts
+++ src/routes/emailVerification.router.ts
@@ -14,6 +14,7 @@ const service = new EmailVerificationService(repo, PrismaInstance)
 const controller = new EmailVerificationController(service)
 
 router.post("/request-otp", validate(requestOtpValidation), controller.requestOtp);
+router.post("/resend-otp", validate(requestOtpValidation), controller.resendOtpPublic);
 router.post("/verify-otp", validate(verifyOtpValidation), controller.verifyOtp);
 
 export default router;
